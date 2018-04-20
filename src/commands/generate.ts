9f7a645eb74b8203e33392d2cbde6c01c06f5b81
Sam Deere <dev@colophonemes.com>
2018-04-20 15:30:36 -0700

    Add template file generator

---
+++ src/commands/generate.ts
@@ -0,0 +1,38 @@
+import {Command, flags} from '@oclif/command'
+import * as inquirer from 'inquirer'
+import * as path from 'path'
+import {fs} from 'mz'
+
+const TEMPLATE_FILE_PATH = path.join(__dirname, '..', 'generator', 'template.js')
+
+export default class Generate extends Command {
+  static description = 'generate a new mail merge template in the current working directory'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    fileName: flags.string({char: 'f', description: 'the output filename', default: 'template.js'}),
+  }
+
+  async run() {
+    const {flags} = this.parse(Generate)
+
+    let fileName = flags.fileName || 'template.js'
+
+    // add file extension if needed
+    if (!/\.js$/.test(fileName)) fileName = `${fileName}.js`
+    // copy our template to the CWD
+    const templateFile = await fs.readFile(TEMPLATE_FILE_PATH)
+    const OUTPUT_FILE_PATH = path.join(process.cwd(), fileName)
+    try {
+      // write the file if it doesn't exist
+      await fs.writeFile(OUTPUT_FILE_PATH, templateFile, { flag: 'wx' })
+      this.log(`Generated template file at ${OUTPUT_FILE_PATH}`)
+    } catch (err) {
+      if (err.code === 'EEXIST') {
+        this.error(`${OUTPUT_FILE_PATH} already exists. Use a different filename or delete the file`)
+      } else {
+        throw err
+      }
+    }
+  }
+}
---
+++ src/generator/template.js
@@ -0,0 +1,28 @@
+// FROM
+const from = 'test@example.com'
+const fromName = 'Test Sender'
+
+// TO
+const to = (row) => `${row.email}`
+const toName = (row) => `${row.firstName} ${row.lastName}`
+
+// SUBJECT
+const subject = (row) => `Testing, testing, 123 🎙`
+
+// MESSAGE
+function message (row) {
+  return (`
+
+<p>Dear ${row.firstName},</p>
+
+<p>Lorem ipsom dolor sit amet.</p>
+
+<p>
+  Thanks,<br />
+  Test Sender
+</p>
+
+`).trim()
+}
+
+module.exports = {from, fromName, to, toName, subject, message}
